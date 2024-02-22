196ca918afc99cd77a891be2d1afc17ef2859b11
jwjeong <timestar7979@gmail.com>
2024-02-22 23:32:42 +0900

    feat: 프로젝트 생성 및 세팅
    1. App.js 수정
    - BrowserRouter 폼 수정

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Main from "./pages/Main";
 import Join from "./pages/Join";
 import MovieList from "./pages/MovieList";
 import SignIn from "./pages/SignIn";
 import TimetableList from "./pages/TimetableList";
 import MyPage from "./pages/MyPage";
-import Error from "./pages/Error";
-import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 function App() {
-  const router = createBrowserRouter([
-    { path: "/", element: <Main />, errorElement: <Error /> },
-    { path: "/join", element: <Join />, errorElement: <Error /> },
-    { path: "/signIn", element: <SignIn />, errorElement: <Error /> },
-    { path: "/movie", element: <MovieList />, errorElement: <Error /> },
-    { path: "/timetable", element: <TimetableList />, errorElement: <Error /> },
-    { path: "/myPage", element: <MyPage />, errorElement: <Error /> },
-  ]);
-
   return (
       <div>
-        <Header />
-        <RouterProvider router={router} />
-        <Footer />
+          <BrowserRouter>
+              <Routes>
+                  <Route exact path="/" element={<Main/>} />
+                  <Route exact path="/join" element={<Join/>} />
+                  <Route exact path="/signIn" element={<SignIn/>} />
+                  <Route exact path="/movie" element={<MovieList/>} />
+                  <Route exact path="/timetable" element={<TimetableList/>} />
+                  <Route exact path="/myPage" element={<MyPage/>} />
+              </Routes>
+              <Footer />
+          </BrowserRouter>
       </div>
   );
 }
