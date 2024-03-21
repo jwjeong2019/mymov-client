3abb7f2089f20263079f41dfa3f7031d51c7f1ff
jwjeong <timestar7979@gmail.com>
2024-03-22 03:17:23 +0900

    modify: 컴포넌트 수정
    1. 공통 CSS 추가
    - App.css
    2. 우측 메뉴 간격 및 폰트 변경
    - Navigation.js, Navigation.css

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -12,31 +12,31 @@ const Navigation = (props) => {
     return (
         <div className="nav-container">
             <div className="nav-box-big">
-                <div className="nav-box-small">
+                <div className="nav-box-small-left">
                     <div className="nav-box-small-circle" onClick={onClickLogo}/>
                 </div>
-                <div className="nav-box-small">
-                    <div className="nav-box-small-link">
+                <div className="nav-box-small-right">
+                    <div className="nav-box-small-link font-HakMulB">
                         <Link to="/movie">영화</Link>
                     </div>
-                    <div className="nav-box-small-link">
+                    <div className="nav-box-small-link font-HakMulB">
                         <Link to="/timetable">상영표</Link>
                     </div>
                     {role === 'ADMIN' &&
-                        <div className="nav-box-small-link">
+                        <div className="nav-box-small-link font-HakMulB">
                             <Link to="/admin/management/movie/list">관리하기</Link>
                         </div>
                     }
                     {(role && name) ?
-                        <div className="nav-box-small-link">
-                            <button onClick={onClickButtonName}>{name} 님</button>
+                        <div className="nav-box-small-link font-HakMulB">
+                            <button className="font-HakMulB" onClick={onClickButtonName}>{name} 님</button>
                         </div>
                     :
                     <>
-                        <div className="nav-box-small-link">
+                        <div className="nav-box-small-link font-HakMulB">
                             <Link to="/signIn">로그인</Link>
                         </div>
-                        <div className="nav-box-small-link">
+                        <div className="nav-box-small-link font-HakMulB">
                             <Link to="/join">회원가입</Link>
                         </div>
                     </>
