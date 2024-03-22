d5fd4f9938cbd133ef16d16dff20f110b627c83c
jwjeong <timestar7979@gmail.com>
2024-03-23 06:11:13 +0900

    modify: 컴포넌트 수정
    1. 로그인 유저의 role에 따른 분기 처리
    - SideBar.js

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -4,12 +4,14 @@ import {Link} from "react-router-dom";
 import {useNavigate} from "react-router";
 
 const SideBar = (props) => {
-    let containerInlineStyle = {
-        display: props.isOpen ? 'block' : 'none'
-    }
     const name = localStorage.getItem('name');
     const role = localStorage.getItem('role');
-
+    let containerInlineStyle = {
+        display: props.isOpen ? 'block' : 'none'
+    };
+    let boxInlineStyle = {
+        height: role === 'ADMIN' && '8%'
+    };
     const navigate = useNavigate();
     const onClickButtonBack = () => props.toggleIsOpen();
     const onClickLogout = () => {
@@ -20,7 +22,7 @@ const SideBar = (props) => {
 
     return (
         <div className="sidebar-container" style={containerInlineStyle}>
-            <div className="sidebar-box-depth-1">
+            <div className="sidebar-box-depth-1" style={boxInlineStyle}>
                 <div className="sidebar-box-depth-2-top">
                     <div className="sidebar-box-depth-3-icon">
                         <button onClick={onClickButtonBack}>
@@ -37,22 +39,26 @@ const SideBar = (props) => {
                     <div className="sidebar-box-depth-3-logout font-TAEBAEK" onClick={onClickLogout}>로그아웃</div>
                 </div>
                 <div className="sidebar-box-depth-2-line"/>
-                <div className="sidebar-box-depth-2-bottom">
-                    <div className="sidebar-box-depth-3-icon">
-                        <IoTicketOutline />
-                    </div>
-                    <div className="sidebar-box-depth-3-text font-TAEBAEK">
-                        <Link to="/myPage/reservation">예매현황</Link>
-                    </div>
-                </div>
-                <div className="sidebar-box-depth-2-bottom">
-                    <div className="sidebar-box-depth-3-icon">
-                        <IoAccessibility />
-                    </div>
-                    <div className="sidebar-box-depth-3-text font-TAEBAEK">
-                        <Link to="/myPage/modify">개인정보변경</Link>
-                    </div>
-                </div>
+                {role === 'USER' &&
+                    <>
+                        <div className="sidebar-box-depth-2-bottom">
+                            <div className="sidebar-box-depth-3-icon">
+                                <IoTicketOutline />
+                            </div>
+                            <div className="sidebar-box-depth-3-text font-TAEBAEK">
+                                <Link to="/myPage/reservation">예매현황</Link>
+                            </div>
+                        </div>
+                        <div className="sidebar-box-depth-2-bottom">
+                            <div className="sidebar-box-depth-3-icon">
+                                <IoAccessibility />
+                            </div>
+                            <div className="sidebar-box-depth-3-text font-TAEBAEK">
+                                <Link to="/myPage/modify">개인정보변경</Link>
+                            </div>
+                        </div>
+                    </>
+                }
             </div>
         </div>
     )
