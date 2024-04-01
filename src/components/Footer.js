ff967fd9bac76840f079850f60e2f4dba8e429ee
jwjeong <timestar7979@gmail.com>
2024-04-02 00:06:50 +0900

    modify: 컴포넌트 수정
    1. 공통 CSS 추가
    - App.css
    2. 컴포넌트 수정
    - Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,12 @@
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer>
+            <text className="font-HakDotR">MY MOVIE</text>
+            <div>
+                <p>Copyright © 2024 mymovie.co.,Ltd. All rights reserved.</p>
+                <address>Contact webmaster for more information. 070-1234-5678</address>
+            </div>
+        </footer>
     )
 }
 
