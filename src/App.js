ed085e22c820ef35caecd1278fc3cb32886b7564
jwjeong <timestar7979@gmail.com>
2024-05-13 21:26:50 +0900

    feat: 라우터
    - 부제: 라우터 수정
    - 설명: 기존 Route 제거 및 새 Route 추가

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Main from "./pages/Main";
-import Join from "./pages/Join";
-import MovieList from "./pages/MovieList";
-import SignIn from "./pages/SignIn";
-import Timetable from "./pages/Timetable";
-import MyPage from "./pages/MyPage";
 import Footer from "./components/Footer";
-import MovieDetail from "./pages/MovieDetail";
-import Reservation from "./pages/Reservation";
-import Management from "./pages/Management";
-import FindIdPwd from "./pages/FindIdPwd";
+import AdminMain from "./admin/page/AdminMain";
+import AdminManagementGenre from "./admin/page/AdminManagementGenre";
 
 function App() {
   return (
@@ -19,15 +13,9 @@ function App() {
           <BrowserRouter>
               <Routes>
                   <Route exact path="/" element={<Main/>} />
-                  <Route exact path="/join" element={<Join/>} />
-                  <Route exact path="/signIn" element={<SignIn/>} />
-                  <Route exact path="/movie" element={<MovieList/>} />
-                  <Route exact path="/timetable" element={<Timetable/>} />
-                  <Route exact path="/myPage/:step" element={<MyPage/>} />
-                  <Route exact path="/movie/detail/:id" element={<MovieDetail/>} />
-                  <Route exact path="/reservation/:mode" element={<Reservation/>} />
-                  <Route path="/admin/management/:step/:mode" element={<Management/>} />
-                  <Route path="/findIdPwd" element={<FindIdPwd/>} />
+                  <Route path={'/admin'} element={<AdminMain />}>
+                      <Route path={'management/genre'} element={<AdminManagementGenre />} />
+                  </Route>
               </Routes>
               <Footer />
           </BrowserRouter>
