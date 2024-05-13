4add5a27d5938b9909c678b372043a6bbd2553f2
jwjeong <timestar7979@gmail.com>
2024-05-13 21:47:28 +0900

    design: 관리자 장르 관리 화면
    - 부제: UI/UX
    - 설명: 등록 버튼, 모달 추가

--- src/admin/page/AdminManagementGenre.js
+++ src/admin/page/AdminManagementGenre.js
@@ -6,7 +6,7 @@ import {
     Dropdown,
     DropdownButton,
     Form,
-    InputGroup, Pagination,
+    InputGroup, Modal, Pagination,
     Row,
     Stack,
     Table
@@ -15,80 +15,100 @@ import {IoIosSearch} from "react-icons/io";
 
 const AdminManagementGenre = () => {
     return (
-        <Container>
-            <Row>
-                <Col>
-                    <Card>
-                        <Card.Body>
-                            <Card.Title>Conditions</Card.Title>
-                            <Stack direction={'horizontal'} gap={3}>
-                                <DropdownButton variant={'outline-dark'} title={'전체'}>
-                                    <Dropdown.Item>전체</Dropdown.Item>
-                                </DropdownButton>
-                                <InputGroup>
-                                    <Form.Control />
-                                    <Button variant={'dark'}><IoIosSearch className={'h3 m-0'} /></Button>
-                                </InputGroup>
-                            </Stack>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-            <Row className={'mt-3'}>
-                <Col>
-                    <Card>
-                        <Card.Body>
-                            <Card.Title>Result</Card.Title>
-                            <Stack>
-                                <DropdownButton className={'d-flex justify-content-end'} variant={'outline-dark'} title={'전체'}>
-                                    <Dropdown.Item>이름순</Dropdown.Item>
-                                </DropdownButton>
-                            </Stack>
-                            <Stack className={'mt-3'}>
-                                <Table striped bordered hover>
-                                    <thead>
-                                        <tr>
-                                            <th>#</th>
-                                            <th>장르명</th>
-                                            <th>삭제</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>1</td>
-                                            <td>Family</td>
-                                            <td><Button variant={'danger'}>삭제</Button></td>
-                                        </tr>
-                                        <tr>
-                                            <td>1</td>
-                                            <td>Family</td>
-                                            <td><Button variant={'danger'}>삭제</Button></td>
-                                        </tr>
-                                        <tr>
-                                            <td>1</td>
-                                            <td>Family</td>
-                                            <td><Button variant={'danger'}>삭제</Button></td>
-                                        </tr>
-                                    </tbody>
-                                    <tfoot>
-                                        <tr>
-                                            <td colSpan={3}>
-                                                <Pagination className={'justify-content-center'}>
-                                                    <Pagination.Prev />
-                                                    <Pagination.Item>1</Pagination.Item>
-                                                    <Pagination.Item>2</Pagination.Item>
-                                                    <Pagination.Next />
-                                                </Pagination>
-                                            </td>
-                                        </tr>
-                                    </tfoot>
-                                </Table>
-                            </Stack>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-        </Container>
+        <>
+            <Container>
+                <Row>
+                    <Col>
+                        <Card>
+                            <Card.Body>
+                                <Card.Title>Conditions</Card.Title>
+                                <Stack direction={'horizontal'} gap={3}>
+                                    <DropdownButton variant={'outline-dark'} title={'전체'}>
+                                        <Dropdown.Item>전체</Dropdown.Item>
+                                    </DropdownButton>
+                                    <InputGroup>
+                                        <Form.Control />
+                                        <Button variant={'dark'}><IoIosSearch className={'h3 m-0'} /></Button>
+                                    </InputGroup>
+                                </Stack>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+                <Row className={'mt-3'}>
+                    <Col>
+                        <Card>
+                            <Card.Body>
+                                <Card.Title>Result</Card.Title>
+                                <Stack className={'justify-content-end'} direction={'horizontal'} gap={2}>
+                                    <Button variant={'dark'}>등록</Button>
+                                    <DropdownButton variant={'outline-dark'} title={'전체'}>
+                                        <Dropdown.Item>이름순</Dropdown.Item>
+                                    </DropdownButton>
+                                </Stack>
+                                <Stack className={'mt-3'}>
+                                    <Table striped bordered hover>
+                                        <thead>
+                                            <tr>
+                                                <th>#</th>
+                                                <th>장르명</th>
+                                                <th>삭제</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>1</td>
+                                                <td>Family</td>
+                                                <td><Button variant={'danger'}>삭제</Button></td>
+                                            </tr>
+                                            <tr>
+                                                <td>1</td>
+                                                <td>Family</td>
+                                                <td><Button variant={'danger'}>삭제</Button></td>
+                                            </tr>
+                                            <tr>
+                                                <td>1</td>
+                                                <td>Family</td>
+                                                <td><Button variant={'danger'}>삭제</Button></td>
+                                            </tr>
+                                        </tbody>
+                                        <tfoot>
+                                            <tr>
+                                                <td colSpan={3}>
+                                                    <Pagination className={'justify-content-center'}>
+                                                        <Pagination.Prev />
+                                                        <Pagination.Item>1</Pagination.Item>
+                                                        <Pagination.Item>2</Pagination.Item>
+                                                        <Pagination.Next />
+                                                    </Pagination>
+                                                </td>
+                                            </tr>
+                                        </tfoot>
+                                    </Table>
+                                </Stack>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+            <Modal show={false}>
+                <Modal.Header className={'bg-dark text-light'}>
+                    <Modal.Title>장르 등록하기</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group>
+                            <Form.Label>장르명</Form.Label>
+                            <Form.Control />
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant={'dark'}>완료</Button>
+                    <Button variant={'outline-dark'}>닫기</Button>
+                </Modal.Footer>
+            </Modal>
+        </>
     );
 };
 
