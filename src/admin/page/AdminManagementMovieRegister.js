d6a2fe97d4dad8dd74be14772a069d8555920e21
jwjeong <timestar7979@gmail.com>
2024-05-13 22:56:47 +0900

    design: 관리자 영화 관리 등록 화면
    - 부제: UI/UX
    - 설명: 전체 퍼블리싱

---
+++ src/admin/page/AdminManagementMovieRegister.js
@@ -0,0 +1,129 @@
+import {Button, Card, Col, Container, Form, Image, Row, Stack} from "react-bootstrap";
+
+const AdminManagementMovieRegister = () => {
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Details</Card.Title>
+                            <Container className={'mt-4'} fluid>
+                                <Row>
+                                    <Col md={3}>
+                                        <Image className={'w-100'} src={'https://cdn.pixabay.com/photo/2024/05/05/05/55/goose-8740266_1280.jpg'} />
+                                    </Col>
+                                </Row>
+                                <Row className={'mt-3'}>
+                                    <Col>
+                                        <Form>
+                                            <Form.Group>
+                                                <Form.Label>제목</Form.Label>
+                                                <Form.Control />
+                                            </Form.Group>
+                                        </Form>
+                                    </Col>
+                                    <Col>
+                                        <Form>
+                                            <Form.Group>
+                                                <Form.Label>연령</Form.Label>
+                                                <Form.Control />
+                                            </Form.Group>
+                                        </Form>
+                                    </Col>
+                                    <Col>
+                                        <Form>
+                                            <Form.Group>
+                                                <Form.Label>감독</Form.Label>
+                                                <Form.Control />
+                                            </Form.Group>
+                                        </Form>
+                                    </Col>
+                                </Row>
+                                <Row className={'mt-3'}>
+                                    <Col>
+                                        <Form>
+                                            <Form.Group>
+                                                <Form.Label>설명</Form.Label>
+                                                <Form.Control as={'textarea'} />
+                                            </Form.Group>
+                                        </Form>
+                                    </Col>
+                                </Row>
+                                <Row className={'mt-3'}>
+                                    <Col>
+                                        <Form>
+                                            <Form.Group>
+                                                <Form.Label>시간</Form.Label>
+                                                <Form.Control type={'time'} />
+                                            </Form.Group>
+                                        </Form>
+                                    </Col>
+                                    <Col>
+                                        <Form>
+                                            <Form.Group>
+                                                <Form.Label>개봉일</Form.Label>
+                                                <Form.Control type={'date'} />
+                                            </Form.Group>
+                                        </Form>
+                                    </Col>
+                                    <Col>
+                                        <Form>
+                                            <Form.Group>
+                                                <Form.Label>상영일</Form.Label>
+                                                <Form.Control type={'date'} />
+                                            </Form.Group>
+                                        </Form>
+                                    </Col>
+                                </Row>
+                                <Row className={'mt-3'}>
+                                    <Col md={4}>
+                                        <Form>
+                                            <Form.Group>
+                                                <Form.Label>장르</Form.Label>
+                                                <Form.Select>
+                                                    <option value={0}>All</option>
+                                                    <option value={1}>Family</option>
+                                                    <option value={2}>Action</option>
+                                                    <option value={3}>SF</option>
+                                                </Form.Select>
+                                            </Form.Group>
+                                        </Form>
+                                    </Col>
+                                </Row>
+                                <Row className={'mt-3'}>
+                                    <Col>
+                                        <Form>
+                                            <Form.Group as={Row}>
+                                                <Form.Label>첨부파일</Form.Label>
+                                                <Col>
+                                                    <Form.Control />
+                                                </Col>
+                                                <Col>
+                                                    <Form.Group>
+                                                        <Form.Label className={'btn btn-outline-dark'} htmlFor={'file'}>파일찾기</Form.Label>
+                                                        <Form.Control type={'file'} id={'file'} accept={'image/*'} hidden />
+                                                    </Form.Group>
+                                                </Col>
+                                            </Form.Group>
+                                        </Form>
+                                    </Col>
+                                </Row>
+                                <Row className={'mt-3'}>
+                                    <Col>
+                                        <Stack className={'justify-content-end'} direction={'horizontal'} gap={2}>
+                                            <Button variant={'dark'}>완료</Button>
+                                            <Button variant={'outline-dark'}>취소</Button>
+                                        </Stack>
+                                    </Col>
+                                </Row>
+                            </Container>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default AdminManagementMovieRegister;
