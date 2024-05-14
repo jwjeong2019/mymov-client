3acd7d7c5c8d4f741e256c4d17969b92400286f2
jwjeong <timestar7979@gmail.com>
2024-05-14 20:43:09 +0900

    design: 관리자 좌석 관리 화면
    - 부제: UI/UX
    - 설명: 전체 퍼블리싱

---
+++ src/admin/page/AdminManagementSeat.js
@@ -0,0 +1,148 @@
+import {
+    Button,
+    Card,
+    Col,
+    Container,
+    Dropdown,
+    DropdownButton,
+    Form,
+    InputGroup, Modal, Pagination,
+    Row,
+    Stack,
+    Table
+} from "react-bootstrap";
+import {IoIosSearch} from "react-icons/io";
+
+const AdminManagementSeat = () => {
+    return (
+        <>
+            <Container>
+                <Row>
+                    <Col>
+                        <Card>
+                            <Card.Body>
+                                <Card.Title>Conditions</Card.Title>
+                                <Stack direction={'horizontal'} gap={3}>
+                                    <DropdownButton variant={'outline-dark'} title={'전체'}>
+                                        <Dropdown.Item>전체</Dropdown.Item>
+                                    </DropdownButton>
+                                    <InputGroup>
+                                        <Form.Control />
+                                        <Button variant={'dark'}><IoIosSearch className={'h3 m-0'} /></Button>
+                                    </InputGroup>
+                                </Stack>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+                <Row className={'mt-3'}>
+                    <Col>
+                        <Card>
+                            <Card.Body>
+                                <Card.Title>Result</Card.Title>
+                                <Stack className={'justify-content-end'} direction={'horizontal'} gap={2}>
+                                    <Button variant={'dark'}>등록</Button>
+                                    <DropdownButton variant={'outline-dark'} title={'전체'}>
+                                        <Dropdown.Item>영화관순</Dropdown.Item>
+                                        <Dropdown.Item>상영관순</Dropdown.Item>
+                                    </DropdownButton>
+                                </Stack>
+                                <Stack className={'mt-3'}>
+                                    <Table striped bordered hover>
+                                        <thead>
+                                            <tr>
+                                                <th>#</th>
+                                                <th>좌석</th>
+                                                <th>상태</th>
+                                                <th>상영관</th>
+                                                <th>영화관</th>
+                                                <th>지역</th>
+                                                <th>삭제</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>1</td>
+                                                <td>A1</td>
+                                                <td>EMPTY</td>
+                                                <td>1상영관</td>
+                                                <td>MOV강남점</td>
+                                                <td>서울</td>
+                                                <td><Button variant={'danger'}>삭제</Button></td>
+                                            </tr>
+                                            <tr>
+                                                <td>1</td>
+                                                <td>A1</td>
+                                                <td>EMPTY</td>
+                                                <td>1상영관</td>
+                                                <td>MOV강남점</td>
+                                                <td>서울</td>
+                                                <td><Button variant={'danger'}>삭제</Button></td>
+                                            </tr>
+                                            <tr>
+                                                <td>1</td>
+                                                <td>A1</td>
+                                                <td>EMPTY</td>
+                                                <td>1상영관</td>
+                                                <td>MOV강남점</td>
+                                                <td>서울</td>
+                                                <td><Button variant={'danger'}>삭제</Button></td>
+                                            </tr>
+                                        </tbody>
+                                        <tfoot>
+                                            <tr>
+                                                <td colSpan={7}>
+                                                    <Pagination className={'justify-content-center'}>
+                                                        <Pagination.Prev />
+                                                        <Pagination.Item>1</Pagination.Item>
+                                                        <Pagination.Item>2</Pagination.Item>
+                                                        <Pagination.Next />
+                                                    </Pagination>
+                                                </td>
+                                            </tr>
+                                        </tfoot>
+                                    </Table>
+                                </Stack>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+            <Modal show={false}>
+                <Modal.Header className={'bg-dark text-light'}>
+                    <Modal.Title>좌석 등록하기</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group>
+                            <Form.Label>영화관</Form.Label>
+                            <Form.Select>
+                                <option value={0}>전체</option>
+                                <option value={1}>MOV강남점</option>
+                                <option value={2}>MOV홍대점</option>
+                            </Form.Select>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>상영관</Form.Label>
+                            <Form.Select>
+                                <option value={0}>전체</option>
+                                <option value={1}>1상영관</option>
+                                <option value={2}>2상영관</option>
+                            </Form.Select>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>좌석명</Form.Label>
+                            <Form.Control />
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant={'dark'}>완료</Button>
+                    <Button variant={'outline-dark'}>닫기</Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+};
+
+export default AdminManagementSeat;
