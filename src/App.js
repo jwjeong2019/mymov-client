047be4f1c050333a4a453e140b5ec664c3900c7e
jwjeong <timestar7979@gmail.com>
2024-05-14 21:10:20 +0900

    feat: 라우터
    - 부제: 라우터 추가
    - 설명: 관리자 영화 관리 등록, 상세, 영화관 관리, 상영관 관리, 좌석 관리, 상영표 관리 Route 추가

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ import Footer from "./components/Footer";
 import AdminMain from "./admin/page/AdminMain";
 import AdminManagementGenre from "./admin/page/AdminManagementGenre";
 import AdminManagementMovie from "./admin/page/AdminManagementMovie";
+import AdminManagementMovieRegister from "./admin/page/AdminManagementMovieRegister";
+import AdminManagementMovieDetail from "./admin/page/AdminManagementMovieDetail";
+import AdminManagementCinema from "./admin/page/AdminManagementCinema";
+import AdminManagementTheater from "./admin/page/AdminManagementTheater";
+import AdminManagementSeat from "./admin/page/AdminManagementSeat";
+import AdminManagementTimetable from "./admin/page/AdminManagementTimetable";
 
 function App() {
   return (
@@ -17,6 +23,12 @@ function App() {
                   <Route path={'/admin'} element={<AdminMain />}>
                       <Route path={'management/genre'} element={<AdminManagementGenre />} />
                       <Route path={'management/movie'} element={<AdminManagementMovie />} />
+                      <Route path={'management/movie/register'} element={<AdminManagementMovieRegister />} />
+                      <Route path={'management/movie/:id'} element={<AdminManagementMovieDetail />} />
+                      <Route path={'management/cinema'} element={<AdminManagementCinema />} />
+                      <Route path={'management/theater'} element={<AdminManagementTheater />} />
+                      <Route path={'management/seat'} element={<AdminManagementSeat />} />
+                      <Route path={'management/timetable'} element={<AdminManagementTimetable />} />
                   </Route>
               </Routes>
               <Footer />
