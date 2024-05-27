085e519edb8da30e5f7bdcbcd574a328bebb0c8c
jwjeong <timestar7979@gmail.com>
2024-05-28 08:50:04 +0900

    feat: 관리자 메인 화면
    - 부제: 기능 구현
    - 설명: 관리자 이름 세팅, 사이드 메뉴 세팅, 메뉴 제목 세팅

--- src/admin/page/AdminMain.js
+++ src/admin/page/AdminMain.js
@@ -1,18 +1,55 @@
 import {Button, Card, Col, Container, Nav, Navbar, Offcanvas, Row} from "react-bootstrap";
 import {Outlet} from 'react-router-dom';
+import {useLocation, useNavigate} from "react-router";
+import {useMemo, useState} from "react";
 
 const AdminMain = () => {
+    const storageItemName = localStorage.getItem('name');
+    const navigate = useNavigate();
+    const location = useLocation();
+    const [sideMenuItems, setSideMenuItems] = useState([]);
+    const [menuTitle, setMenuTitle] = useState('');
+    const handleClickLogout = () => {
+        localStorage.clear();
+        navigate('/admin/login');
+    };
+    const makeSideMenuItems = () => {
+        const baseURL = '/admin/management';
+        setSideMenuItems([
+            { path: `${baseURL}/genre`, name: '장르 관리하기' },
+            { path: `${baseURL}/movie`, name: '영화 관리하기' },
+            { path: `${baseURL}/cinema`, name: '영화관 관리하기' },
+            { path: `${baseURL}/theater`, name: '상영관 관리하기' },
+            { path: `${baseURL}/seat`, name: '좌석 관리하기' },
+        ]);
+    };
+    const makeMenuTitle = () => {
+        const type = location.pathname.split('/').pop();
+        let titleName = '';
+        switch (type) {
+            case 'genre': titleName = '장르 관리'; break;
+            case 'movie': titleName = '영화 관리'; break;
+            case 'cinema': titleName = '영화관 관리'; break;
+            case 'theater': titleName = '상영관 관리'; break;
+            case 'seat': titleName = '좌석 관리'; break;
+        }
+        setMenuTitle(titleName);
+    };
+    const init = () => {
+        makeSideMenuItems();
+        makeMenuTitle();
+    };
+    useMemo(init, []);
     return (
         <>
             <Navbar bg={'dark'} variant={'dark'} sticky={'top'}>
                 <Container>
                     <Navbar.Brand>Simple Movie</Navbar.Brand>
                     <Navbar.Collapse className={'justify-content-end'}>
+                        <Navbar.Text>{storageItemName}님 반갑습니다.</Navbar.Text>
                         <Nav className={'text-light'}>
-                            <Nav.Link>로그인</Nav.Link>
-                            <Nav.Link>로그아웃</Nav.Link>
+                            <Nav.Link onClick={handleClickLogout}>로그아웃</Nav.Link>
                         </Nav>
-                        <Navbar.Text>관리자님 반갑습니다.</Navbar.Text>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
@@ -25,11 +62,11 @@ const AdminMain = () => {
                             </Card.Header>
                             <Card.Body>
                                 <Nav className={'flex-column'}>
-                                    <Nav.Link className={'text-decoration-none text-light'}>장르 관리하기</Nav.Link>
-                                    <Nav.Link className={'text-decoration-none text-light'}>영화 관리하기</Nav.Link>
-                                    <Nav.Link className={'text-decoration-none text-light'}>영화관 관리하기</Nav.Link>
-                                    <Nav.Link className={'text-decoration-none text-light'}>상영관 관리하기</Nav.Link>
-                                    <Nav.Link className={'text-decoration-none text-light'}>좌석 관리하기</Nav.Link>
+                                    {sideMenuItems.map((item, itemIdx) => {
+                                        return <Nav.Link key={`side-menu-${itemIdx}`}
+                                                         className={'text-decoration-none text-light'}
+                                                         href={item.path}>{item.name}</Nav.Link>;
+                                    })}
                                 </Nav>
                             </Card.Body>
                         </Card>
@@ -38,7 +75,7 @@ const AdminMain = () => {
                         <Row>
                             <Col>
                                 <Container>
-                                    <div style={{ fontSize: 50 }}>장르 관리</div>
+                                    <div style={{ fontSize: 50 }}>{menuTitle}</div>
                                 </Container>
                             </Col>
                         </Row>
