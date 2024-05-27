b7a0c0529bcf8ee8aae41c8bede46d4e9937fc14
jwjeong <timestar7979@gmail.com>
2024-05-27 19:44:32 +0900

    feat: 사용자 홈 화면
    - 부제: 기능 구현
    - 설명: 인기영화,개봉영화 리스트 더미데이터 처리, 버튼 이벤트 추가

--- src/user/page/UserHome.js
+++ src/user/page/UserHome.js
@@ -1,4 +1,7 @@
 import {Badge, Button, Card, Col, Container, Image, Row, Stack} from "react-bootstrap";
+import {useMemo, useState} from "react";
+import apiMovie from "../../api/apiMovie";
+import {useNavigate} from "react-router";
 
 const UserHome = () => {
     const titleStyle = {
@@ -6,6 +9,33 @@ const UserHome = () => {
         backgroundRepeat: 'no-repeat',
         backgroundImage: 'linear-gradient(to bottom, rgba(255, 255, 255, 0),  rgba(255, 255, 255, 1)), url("https://cdn.pixabay.com/photo/2022/06/20/14/20/space-7273891_1280.jpg")'
     };
+    const navigate = useNavigate();
+    const [popularMovies, setPopularMovies] = useState([]);
+    const [releasedMovies, setReleasedMovies] = useState([]);
+    const handleClickShowMore = () => navigate('/movies');
+    const getPopularMovies = () => {
+        const _params = {};
+        const apiResult = [
+            { id: 0, title: 'Last hope', score: 4.8, img: 'https://cdn.pixabay.com/photo/2024/05/09/16/35/ai-generated-8751441_1280.png' },
+            { id: 1, title: 'Last hope', score: 4.8, img: 'https://cdn.pixabay.com/photo/2024/05/09/16/35/ai-generated-8751441_1280.png' },
+            { id: 2, title: 'Last hope', score: 4.8, img: 'https://cdn.pixabay.com/photo/2024/05/09/16/35/ai-generated-8751441_1280.png' },
+        ];
+        setPopularMovies(apiResult);
+    };
+    const getReleasedMovies = () => {
+        const _params = {};
+        const apiResult = [
+            { id: 0, title: 'Last One', score: 3.8, img: 'https://cdn.pixabay.com/photo/2023/09/06/08/50/tornado-8236696_1280.jpg' },
+            { id: 1, title: 'Last One', score: 3.8, img: 'https://cdn.pixabay.com/photo/2023/09/06/08/50/tornado-8236696_1280.jpg' },
+            { id: 2, title: 'Last One', score: 3.8, img: 'https://cdn.pixabay.com/photo/2023/09/06/08/50/tornado-8236696_1280.jpg' },
+        ];
+        setReleasedMovies(apiResult);
+    };
+    const init = () => {
+        getPopularMovies();
+        getReleasedMovies();
+    };
+    useMemo(init, []);
     return (
         <Container>
             <Row>
@@ -29,51 +59,25 @@ const UserHome = () => {
                 </Col>
             </Row>
             <Row>
-                <Col md={3}>
-                    <Card>
-                        <Card.Img
-                            variant={'top'}
-                            src={'https://cdn.pixabay.com/photo/2024/05/09/16/35/ai-generated-8751441_1280.png'}
-                            height={300}
-                        />
-                        <Card.Body>
-                            <Card.Title>Last hope</Card.Title>
-                            <Card.Text className={'h4'}>
-                                <Badge pill bg={'dark'}>4.8 / 5</Badge>
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col md={3}>
-                    <Card>
-                        <Card.Img
-                            variant={'top'}
-                            src={'https://cdn.pixabay.com/photo/2024/05/09/16/35/ai-generated-8751441_1280.png'}
-                            height={300}
-                        />
-                        <Card.Body>
-                            <Card.Title>Last hope</Card.Title>
-                            <Card.Text className={'h4'}>
-                                <Badge pill bg={'dark'}>4.8 / 5</Badge>
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col md={3}>
-                    <Card>
-                        <Card.Img
-                            variant={'top'}
-                            src={'https://cdn.pixabay.com/photo/2024/05/09/16/35/ai-generated-8751441_1280.png'}
-                            height={300}
-                        />
-                        <Card.Body>
-                            <Card.Title>Last hope</Card.Title>
-                            <Card.Text className={'h4'}>
-                                <Badge pill bg={'dark'}>4.8 / 5</Badge>
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </Col>
+                {popularMovies.map(movie => {
+                    return (
+                        <Col md={3}>
+                            <Card>
+                                <Card.Img
+                                    variant={'top'}
+                                    src={movie.img}
+                                    height={300}
+                                />
+                                <Card.Body>
+                                    <Card.Title>{movie.title}</Card.Title>
+                                    <Card.Text className={'h4'}>
+                                        <Badge pill bg={'dark'}>{movie.score} / 5</Badge>
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    );
+                })}
             </Row>
             <Row className={'mt-5'} style={titleStyle}>
                 <Col>
@@ -81,55 +85,29 @@ const UserHome = () => {
                 </Col>
             </Row>
             <Row>
-                <Col md={3}>
-                    <Card>
-                        <Card.Img
-                            variant={'top'}
-                            src={'https://cdn.pixabay.com/photo/2023/09/06/08/50/tornado-8236696_1280.jpg'}
-                            height={300}
-                        />
-                        <Card.Body>
-                            <Card.Title>Last hope</Card.Title>
-                            <Card.Text className={'h4'}>
-                                <Badge pill bg={'dark'}>3.5 / 5</Badge>
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col md={3}>
-                    <Card>
-                        <Card.Img
-                            variant={'top'}
-                            src={'https://cdn.pixabay.com/photo/2023/09/06/08/50/tornado-8236696_1280.jpg'}
-                            height={300}
-                        />
-                        <Card.Body>
-                            <Card.Title>Last hope</Card.Title>
-                            <Card.Text className={'h4'}>
-                                <Badge pill bg={'dark'}>3.5 / 5</Badge>
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col md={3}>
-                    <Card>
-                        <Card.Img
-                            variant={'top'}
-                            src={'https://cdn.pixabay.com/photo/2023/09/06/08/50/tornado-8236696_1280.jpg'}
-                            height={300}
-                        />
-                        <Card.Body>
-                            <Card.Title>Last hope</Card.Title>
-                            <Card.Text className={'h4'}>
-                                <Badge pill bg={'dark'}>3.5 / 5</Badge>
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </Col>
+                {releasedMovies.map(movie => {
+                    return (
+                        <Col md={3}>
+                            <Card>
+                                <Card.Img
+                                    variant={'top'}
+                                    src={movie.img}
+                                    height={300}
+                                />
+                                <Card.Body>
+                                    <Card.Title>{movie.title}</Card.Title>
+                                    <Card.Text className={'h4'}>
+                                        <Badge pill bg={'dark'}>{movie.score} / 5</Badge>
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    );
+                })}
             </Row>
             <Row className={'mt-5'}>
                 <Col className={'d-flex justify-content-center'}>
-                    <Button variant={'dark'}>Show More</Button>
+                    <Button variant={'dark'} onClick={handleClickShowMore}>Show More</Button>
                 </Col>
             </Row>
         </Container>
