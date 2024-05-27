4a710ff73746e500f926bf9ea53861d3c83b9c45
jwjeong <timestar7979@gmail.com>
2024-05-27 23:21:35 +0900

    feat: 관리자 로그인 화면
    - 부제: 기능 구현
    - 설명: 버튼 기능 추가 및 API 적용

--- src/admin/page/AdminLogin.js
+++ src/admin/page/AdminLogin.js
@@ -1,6 +1,31 @@
 import {Button, Card, Col, Container, Form, Row, Stack} from "react-bootstrap";
+import {useState} from "react";
+import apiAdmin from "../../api/apiAdmin";
+import {Utils} from "../../utils/Utils";
+import {useNavigate} from "react-router";
 
 const AdminLogin = () => {
+    const navigate = useNavigate();
+    const [inputs, setInputs] = useState({});
+    const handleChangeInputsId = e => setInputs(prevState => ({ ...prevState, id: e.target.value }));
+    const handleChangeInputsPassword = e => setInputs(prevState => ({ ...prevState, password: e.target.value }));
+    const handleClickTryIt = () => createToken();
+    const createToken = () => {
+        const _params = {
+            adminId: inputs.id,
+            adminPw: inputs.password,
+        };
+        apiAdmin.createToken(_params)
+            .then(response => {
+                const { data } = response;
+                if (Utils.isContainedWordFrom('fail', data.msg)) return alert('아이디 또는 비밀번호가 올바르지 않습니다.');
+                localStorage.setItem('auth', JSON.stringify(data.token));
+                localStorage.setItem('name', data.detail.name);
+                localStorage.setItem('role', `ADMIN_${data.detail.role}`);
+                navigate('/admin');
+            })
+            .catch(err => alert(`error: ${err.message}`));
+    };
     return (
         <Container className={'font-TAEBAEK'}>
             <Row className={'mt-5'}>
@@ -18,14 +43,14 @@ const AdminLogin = () => {
                         <Form>
                             <Form.Group>
                                 <Form.Label>ID</Form.Label>
-                                <Form.Control />
+                                <Form.Control onChange={handleChangeInputsId} />
                             </Form.Group>
                             <Form.Group className={'mt-3'}>
                                 <Form.Label>PASSWORD</Form.Label>
-                                <Form.Control />
+                                <Form.Control type={'password'} onChange={handleChangeInputsPassword} />
                             </Form.Group>
                         </Form>
-                        <Button className={'mt-5'} variant={'dark'}>
+                        <Button className={'mt-5'} variant={'dark'} onClick={handleClickTryIt}>
                             <div className={'h3 m-0 fw-bold'}>Try It</div>
                         </Button>
                     </Stack>
