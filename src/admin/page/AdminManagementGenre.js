0c5f7e7f3f2056e9be89570d4f9fe6b7c3ccc980
jwjeong <timestar7979@gmail.com>
2024-05-28 22:02:44 +0900

    feat: 관리자 장르 관리 화면
    - 부제: 기능 구현
    - 설명: 등록하기 및 리스트 표출 API 적용, 검색 기능 구현, 리스트 테이블 기능 구현, 모달 기능 구현

--- src/admin/page/AdminManagementGenre.js
+++ src/admin/page/AdminManagementGenre.js
@@ -12,8 +12,134 @@ import {
     Table
 } from "react-bootstrap";
 import {IoIosSearch} from "react-icons/io";
+import {useMemo, useState} from "react";
+import apiAdmin from "../../api/apiAdmin";
+import {Utils} from "../../utils/Utils";
+import apiGenre from "../../api/apiGenre";
+import CustomTable from "../component/CustomTable";
 
 const AdminManagementGenre = () => {
+    const storageItemAuth = JSON.parse(localStorage.getItem('auth'));
+    const [isShow, setIsShow] = useState(false);
+    const [modalInputs, setModalInputs] = useState({});
+    const [searchData, setSearchData] = useState({});
+    const [tableHeaders, setTableHeaders] = useState([]);
+    const [genres, setGenres] = useState([]);
+    const [tablePage, setTablePage] = useState({});
+    const [filters, setFilters] = useState([]);
+    const [sorts, setSorts] = useState([]);
+    const [currentFilter, setCurrentFilter] = useState({});
+    const [currentSort, setCurrentSort] = useState({});
+    const handleClickRegister = () => setIsShow(true);
+    const handleClickModalClose = () => setIsShow(false);
+    const handleClickModalComplete = () => createGenre();
+    const handleChangeModalInputsGenreName = e => setModalInputs(prevState => ({ ...prevState, genreName: e.target.value }));
+    const handleClickDelete = e => {
+        const isOk = window.confirm('삭제 하시겠습니까?');
+        if (isOk) deleteGenre(e.target.value);
+    };
+    const handleClickTablePage = number => getGenres(number);
+    const handleChangeSearchDataKeyword = e => setSearchData(prevState => ({ ...prevState, keyword: e.target.value }));
+    const handleChangeSearchDataFilter = filter => {
+        setSearchData(prevState => ({...prevState, filter: filter.value}));
+        setCurrentFilter(filter);
+    };
+    const handleChangeSearchDataSort = sort => {
+        const _nextSearchData = { ...searchData, sort: sort.value };
+        setSearchData(_nextSearchData);
+        setCurrentSort(sort);
+        getGenres(tablePage.page, _nextSearchData);
+    };
+    const handleClickSearch = () => getGenres(1, searchData);
+    const createGenre = () => {
+        const _params = {
+            name: modalInputs.genreName,
+            grantType: storageItemAuth.grantType,
+            accessToken: storageItemAuth.accessToken
+        };
+        apiAdmin.createGenre(_params)
+            .then(response => {
+                const { data } = response;
+                if (Utils.isContainedWordFrom('fail', data.msg)) return alert(`장르 생성 실패:\n${data.msg}`);
+                if (Utils.isContainedWordFrom('already', data.msg)) return alert(`장르 생성 실패:\n${data.msg}`);
+                if (Utils.isContainedWordFrom('authority', data.msg)) return alert(`권한 실패:\n${data.msg}`);
+                alert('장르를 정상적으로 등록하였습니다.');
+                setIsShow(false);
+                window.location.reload();
+            })
+            .catch(err => alert(`error: ${err.message}`));
+    };
+    const getGenres = (page, search) => {
+        const _params = {
+            page: page - 1,
+            size: 10,
+            grantType: storageItemAuth.grantType,
+            accessToken: storageItemAuth.accessToken,
+            keyword: search?.keyword,
+            keywordField: search?.filter,
+            sortField: search?.sort,
+            sortType: 'DESC'
+        };
+        apiGenre.getList(_params)
+            .then(response => {
+                const { data } = response;
+                if (data.result.totalElements > 0) {
+                    const _genres = data.result.content.map(genre => ({
+                        id: genre.id,
+                        name: genre.name,
+                        button: <Button variant={'danger'}
+                                        value={genre.id}
+                                        onClick={handleClickDelete}>삭제</Button>,
+                    }));
+                    setGenres(_genres);
+                }
+                setTablePage(prevState => ({
+                    ...prevState,
+                    page,
+                    size: 10,
+                    total: data.result.totalElements ?? 0
+                }));
+            })
+            .catch(err => alert(`error: ${err.message}`));
+    };
+    const deleteGenre = id => {
+        const _params = {
+            grantType: storageItemAuth.grantType,
+            accessToken: storageItemAuth.accessToken,
+            id
+        };
+        apiAdmin.deleteGenre(_params)
+            .then(response => {
+                const { data } = response;
+                if (Utils.isContainedWordFrom('fail', data.msg)) return alert(`장르 삭제 실패:\n${data.msg}`);
+                if (Utils.isContainedWordFrom('authority', data.msg)) return alert(`권한 실패:\n${data.msg}`);
+                alert('장르를 정상적으로 삭제하였습니다.');
+                window.location.reload();
+            })
+            .catch(err => alert(`error: ${err.message}`));
+    }
+    const makeTableHeaders = () => {
+        setTableHeaders([ '#', '장르명', '삭제' ]);
+    };
+    const makeFilters = () => {
+        setFilters([
+            { title: '전체' },
+            { value: 'NAME', title: '장르명' },
+        ]);
+    };
+    const makeSorts = () => {
+        setSorts([
+            { title: '전체' },
+            { value: 'name', title: '이름순' },
+        ]);
+    };
+    const init = () => {
+        makeTableHeaders();
+        makeFilters();
+        makeSorts();
+        getGenres(1);
+    };
+    useMemo(init, []);
     return (
         <>
             <Container>
@@ -23,12 +149,17 @@ const AdminManagementGenre = () => {
                             <Card.Body>
                                 <Card.Title>Conditions</Card.Title>
                                 <Stack direction={'horizontal'} gap={3}>
-                                    <DropdownButton variant={'outline-dark'} title={'전체'}>
-                                        <Dropdown.Item>전체</Dropdown.Item>
+                                    <DropdownButton variant={'outline-dark'} title={currentFilter.title ?? '전체'}>
+                                        {filters.map((filter, filterIdx) => {
+                                            return <Dropdown.Item key={`dropdown-item-filter-${filterIdx}`}
+                                                                  onClick={() => handleChangeSearchDataFilter(filter)}>{filter.title}</Dropdown.Item>;
+                                        })}
                                     </DropdownButton>
                                     <InputGroup>
-                                        <Form.Control />
-                                        <Button variant={'dark'}><IoIosSearch className={'h3 m-0'} /></Button>
+                                        <Form.Control onChange={handleChangeSearchDataKeyword} />
+                                        <Button variant={'dark'} onClick={handleClickSearch}>
+                                            <IoIosSearch className={'h3 m-0'} />
+                                        </Button>
                                     </InputGroup>
                                 </Stack>
                             </Card.Body>
@@ -41,57 +172,28 @@ const AdminManagementGenre = () => {
                             <Card.Body>
                                 <Card.Title>Result</Card.Title>
                                 <Stack className={'justify-content-end'} direction={'horizontal'} gap={2}>
-                                    <Button variant={'dark'}>등록</Button>
-                                    <DropdownButton variant={'outline-dark'} title={'전체'}>
-                                        <Dropdown.Item>이름순</Dropdown.Item>
+                                    <Button variant={'dark'} onClick={handleClickRegister}>등록</Button>
+                                    <DropdownButton variant={'outline-dark'} title={currentSort.title ?? '전체'}>
+                                        {sorts.map((sort, sortIdx) => {
+                                            return <Dropdown.Item key={`dropdown-item-sort-${sortIdx}`}
+                                                                  onClick={() => handleChangeSearchDataSort(sort)}>{sort.title}</Dropdown.Item>;
+                                        })}
                                     </DropdownButton>
                                 </Stack>
                                 <Stack className={'mt-3'}>
-                                    <Table striped bordered hover>
-                                        <thead>
-                                            <tr>
-                                                <th>#</th>
-                                                <th>장르명</th>
-                                                <th>삭제</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <td>1</td>
-                                                <td>Family</td>
-                                                <td><Button variant={'danger'}>삭제</Button></td>
-                                            </tr>
-                                            <tr>
-                                                <td>1</td>
-                                                <td>Family</td>
-                                                <td><Button variant={'danger'}>삭제</Button></td>
-                                            </tr>
-                                            <tr>
-                                                <td>1</td>
-                                                <td>Family</td>
-                                                <td><Button variant={'danger'}>삭제</Button></td>
-                                            </tr>
-                                        </tbody>
-                                        <tfoot>
-                                            <tr>
-                                                <td colSpan={3}>
-                                                    <Pagination className={'justify-content-center'}>
-                                                        <Pagination.Prev />
-                                                        <Pagination.Item>1</Pagination.Item>
-                                                        <Pagination.Item>2</Pagination.Item>
-                                                        <Pagination.Next />
-                                                    </Pagination>
-                                                </td>
-                                            </tr>
-                                        </tfoot>
-                                    </Table>
+                                    <CustomTable
+                                        headerData={tableHeaders}
+                                        bodyData={genres}
+                                        pageData={tablePage}
+                                        onClickPage={handleClickTablePage}
+                                    />
                                 </Stack>
                             </Card.Body>
                         </Card>
                     </Col>
                 </Row>
             </Container>
-            <Modal show={false}>
+            <Modal show={isShow}>
                 <Modal.Header className={'bg-dark text-light'}>
                     <Modal.Title>장르 등록하기</Modal.Title>
                 </Modal.Header>
@@ -99,13 +201,13 @@ const AdminManagementGenre = () => {
                     <Form>
                         <Form.Group>
                             <Form.Label>장르명</Form.Label>
-                            <Form.Control />
+                            <Form.Control onChange={handleChangeModalInputsGenreName} />
                         </Form.Group>
                     </Form>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button variant={'dark'}>완료</Button>
-                    <Button variant={'outline-dark'}>닫기</Button>
+                    <Button variant={'dark'} onClick={handleClickModalComplete}>완료</Button>
+                    <Button variant={'outline-dark'} onClick={handleClickModalClose}>닫기</Button>
                 </Modal.Footer>
             </Modal>
         </>
