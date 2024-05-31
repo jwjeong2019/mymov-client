b3e4cb38681110cc09c4c7007bedf61f0f90ba34
jwjeong <timestar7979@gmail.com>
2024-05-31 20:10:11 +0900

    feat: 사용자 로그인 화면
    - 부제: 기능 구현
    - 설명: 아이디 및 비밀번호 입력, 로그인 구현

--- src/user/page/UserLogin.js
+++ src/user/page/UserLogin.js
@@ -1,6 +1,33 @@
 import {Button, Card, Col, Container, Form, InputGroup, Nav, Row, Stack} from "react-bootstrap";
+import {useState} from "react";
+import apiMember from "../../api/apiMember";
+import {Utils} from "../../utils/Utils";
 
 const UserLogin = () => {
+    const [inputs, setInputs] = useState({});
+    const handleChangeInputsId = e => setInputs(prevState => ({ ...prevState, id: e.target.value }));
+    const handleChangeInputsPassword = e => setInputs(prevState => ({ ...prevState, password: e.target.value }));
+    const handleClickLogin = () => createToken();
+    const createToken = () => {
+        const _params = {
+            memberId: inputs.id,
+            memberPw: inputs.password
+        };
+        apiMember.createToken(_params)
+            .then(response => {
+                const { data } = response;
+                if (Utils.isContainedWordFrom('fail', data.msg)) return alert('아이디 또는 비밀번호가 일치하지 않습니다.');
+                localStorage.setItem('auth', JSON.stringify(data.token));
+                localStorage.setItem('id', data.detail.id);
+                localStorage.setItem('name', data.detail.name);
+                localStorage.setItem('role', 'USER');
+                window.location.href = '/home';
+            })
+            .catch(err => {
+                const { status, data } = err.response;
+                alert(`error: ${data.message} (${status})`);
+            });
+    };
     return (
         <Container>
             <Row className={'mt-5'}>
@@ -14,15 +41,15 @@ const UserLogin = () => {
                                 <Form>
                                     <Form.Group>
                                         <Form.Label>ID</Form.Label>
-                                        <Form.Control placeholder={'Enter your ID'} />
+                                        <Form.Control placeholder={'Enter your ID'} onChange={handleChangeInputsId} />
                                     </Form.Group>
                                     <Form.Group className={'mt-3'}>
                                         <Form.Label>Password</Form.Label>
-                                        <Form.Control type={'password'} placeholder={'Enter your Password'} />
+                                        <Form.Control type={'password'} placeholder={'Enter your Password'} onChange={handleChangeInputsPassword} />
                                     </Form.Group>
                                 </Form>
                                 <Stack className={'mt-5'} gap={2}>
-                                    <Button variant={'dark'}>Log in</Button>
+                                    <Button variant={'dark'} onClick={handleClickLogin}>Log in</Button>
                                     <Stack className={'justify-content-center'} direction={'horizontal'} gap={2}>
                                         <div>Don't have an account?</div>
                                         <Nav.Link className={'fw-bold'} href={'/join'}>Join</Nav.Link>
