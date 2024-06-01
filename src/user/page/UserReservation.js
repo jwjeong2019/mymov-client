cf8916aff6781ed7180da77c120bb8e35d1bb5f1
jwjeong <timestar7979@gmail.com>
2024-06-01 20:00:10 +0900

    feat: 사용자 예매 화면
    - 부제: 기능 구현
    - 설명: 예매할 영화 정보 표출, 상세 옵션 입력, 예매하기 구현

--- src/user/page/UserReservation.js
+++ src/user/page/UserReservation.js
@@ -1,6 +1,168 @@
 import {Button, Card, Col, Container, Form, Image, Row, Stack} from "react-bootstrap";
+import {useLocation, useNavigate} from "react-router";
+import {useMemo, useState} from "react";
+import apiMovie from "../../api/apiMovie";
+import apiCinema from "../../api/apiCinema";
+import apiTheater from "../../api/apiTheater";
+import apiTimetable from "../../api/apiTimetable";
+import apiMember from "../../api/apiMember";
+import {Utils} from "../../utils/Utils";
 
 const UserReservation = () => {
+    const storageItemAuth = JSON.parse(localStorage.getItem('auth'));
+    const location = useLocation();
+    const navigate = useNavigate();
+    const [inputs, setInputs] = useState({});
+    const [movie, setMovie] = useState({});
+    const [cinemas, setCinemas] = useState([]);
+    const [theaters, setTheaters] = useState([]);
+    const [seats, setSeats] = useState([]);
+    const [timetables, setTimetables] = useState([]);
+    const [price, setPrice] = useState(14000);
+    const handleChangeInputsCinema = e => {
+        if (e.target.value !== 'ALL') {
+            const cinema = JSON.parse(e.target.value);
+            setInputs(prevState => ({ ...prevState, cinemaId: cinema.id }));
+            const _nextTheaters = cinema.theaters.map(theater => ({
+                value: theater.id,
+                name: theater.number
+            }));
+            setTheaters(_nextTheaters);
+            return;
+        }
+        setInputs(prevState => ({ ...prevState, cinemaId: undefined }));
+    };
+    const handleChangeInputsTheater = e => {
+        const _theaterId = e.target.value;
+        if (_theaterId !== 'ALL') {
+            setInputs(prevState => ({ ...prevState, theaterId: _theaterId }));
+            getTheater(_theaterId);
+            getTimetables(_theaterId);
+            return;
+        }
+        setInputs(prevState => ({ ...prevState, theaterId: undefined }));
+    };
+    const handleChangeInputsTimetableId = e => {
+        const _timetableId = e.target.value;
+        if (_timetableId !== 'ALL') {
+            setInputs(prevState => ({ ...prevState, timetableId: _timetableId }));
+            return;
+        }
+        setInputs(prevState => ({ ...prevState, timetableId: undefined }));
+    };
+    const handleChangeInputsSeat = e => {
+        const _seatId = e.target.value;
+        if (_seatId !== 'ALL') {
+            setInputs(prevState => ({ ...prevState, seatId: _seatId }));
+            return;
+        }
+        setInputs(prevState => ({ ...prevState, seatId: undefined }));
+    };
+    const handleClickPayment = () => createTicket();
+    const getMovie = () => {
+        const _params = {
+            id: location.state.movieId
+        };
+        apiMovie.getDetail(_params)
+            .then(response => {
+                const { data } = response;
+                setMovie({
+                    id: data.result.id,
+                    title: data.result.title,
+                    age: data.result.age < 12 ? '전체이용가' : `${data.result.age}세`,
+                    director: data.result.director,
+                    releaseDate: data.result.releaseDate.split('T')[0],
+                    runningTime: `${data.result.runningTime}분`,
+                    genres: makeGenres(data.result.genres),
+                    detail: data.result.detail,
+                    imageUrl: data.result.attachment,
+                });
+            })
+            .catch(err => {
+                const { status, data } = err.response;
+                alert(`error: ${data.message} (${status})`);
+            });
+    };
+    const makeGenres = genres => genres.map(genre => genre.name).join(', ');
+    const getCinemas = () => {
+        apiCinema.getList()
+            .then(response => {
+                const { data } = response;
+                const _cinemas = data.result.map(cinema => ({
+                    id: cinema.id,
+                    name: cinema.name,
+                    theaters: cinema.theaters,
+                }));
+                setCinemas(_cinemas);
+            })
+            .catch(err => {
+                const { status, data } = err.response;
+                alert(`error: ${data.message} (${status})`);
+            });
+    };
+    const getTheater = (id) => {
+        const _params = {
+            id,
+        };
+        apiTheater.getDetail(_params)
+            .then(response => {
+                const { data } = response;
+                const _seats = data.result.seats
+                    .filter(seat => seat.status === 'EMPTY')
+                    .map(seat => ({
+                        value: seat.id,
+                        name: seat.position
+                    }));
+                setSeats(_seats);
+            })
+            .catch(err => {
+                const { status, data } = err.response;
+                alert(`error: ${data.message} (${status})`);
+            });
+    };
+    const getTimetables = (theaterId) => {
+        const _params = {
+            movieId: movie.id,
+            cinemaId: inputs.cinemaId,
+            theaterId,
+        };
+        apiTimetable.getListByIds(_params)
+            .then(response => {
+                const { data } = response;
+                const _timetables = data.result.map(timetable => ({
+                    value: timetable.id,
+                    name: timetable.startTime,
+                }));
+                setTimetables(_timetables);
+            })
+            .catch(err => {
+                const { status, data } = err.response;
+                alert(`error: ${data.message} (${status})`);
+            });
+    };
+    const createTicket = () => {
+        const _params = {
+            grantType: storageItemAuth.grantType,
+            accessToken: storageItemAuth.accessToken,
+            timetableId: inputs.timetableId,
+            seatId: inputs.seatId,
+            price,
+        };
+        apiMember.createTicket(_params)
+            .then(response => {
+                const { data } = response;
+                if (Utils.isContainedWordFrom('fail', data.msg)) return alert(`예매 실패:\n${data.msg}`);
+                if (Utils.isContainedWordFrom('already', data.msg)) return alert(`예매 실패:\n${data.msg}`);
+                alert('정상적으로 예매를 완료하였습니다.')
+                navigate(-1);
+            })
+            .catch(err => alert(`ERROR: ${err.message}`));
+    };
+    const init = () => {
+        getMovie();
+        getCinemas();
+    };
+    useMemo(init, []);
     return (
         <Container>
             <Row className={'mt-5'}>
@@ -12,14 +174,14 @@ const UserReservation = () => {
                         <Card.Body>
                             <Card.Title className={'font-HakDotR'}>Movie Info.</Card.Title>
                             <Stack className={'mt-4'} direction={'horizontal'} gap={3}>
-                                <Image src={'https://cdn.pixabay.com/photo/2022/11/17/22/49/city-7599045_1280.jpg'} height={200} />
+                                <Image src={movie.imageUrl} height={200} />
                                 <Stack>
-                                    <div className={'h3'}>Last City</div>
+                                    <div className={'h3'}>{movie.title}</div>
                                     <Stack className={'justify-content-end'}>
-                                        <div>연령: 12</div>
-                                        <div>감독: Benedict Benjamin</div>
-                                        <div>장르: SF</div>
-                                        <div>시간: 120m</div>
+                                        <div>연령: {movie.age}</div>
+                                        <div>감독: {movie.director}</div>
+                                        <div>장르: {movie.genres}</div>
+                                        <div>시간: {movie.runningTime}</div>
                                     </Stack>
                                 </Stack>
                             </Stack>
@@ -29,32 +191,45 @@ const UserReservation = () => {
                                 <Form.Group as={Row}>
                                     <Form.Label column sm={3}>영화관</Form.Label>
                                     <Col>
-                                        <Form.Select>
-                                            <option>전체</option>
+                                        <Form.Select defaultValue={'ALL'} onChange={handleChangeInputsCinema}>
+                                            <option value={'ALL'}>전체</option>
+                                            {cinemas.map((cinema, cinemaIdx) => {
+                                                const valueCinema = JSON.stringify(cinema);
+                                                return <option key={`option-cinema-${cinemaIdx}`} value={valueCinema}>{cinema.name}</option>
+                                            })}
                                         </Form.Select>
                                     </Col>
                                 </Form.Group>
                                 <Form.Group className={'mt-3'} as={Row}>
                                     <Form.Label column sm={3}>상영관</Form.Label>
                                     <Col>
-                                        <Form.Select>
-                                            <option>전체</option>
+                                        <Form.Select defaultValue={'ALL'} onChange={handleChangeInputsTheater}>
+                                            <option value={'ALL'}>전체</option>
+                                            {theaters.map((theater, theaterIdx) => {
+                                                return <option key={`option-theater-${theaterIdx}`} value={theater.value}>{theater.name}상영관</option>
+                                            })}
                                         </Form.Select>
                                     </Col>
                                 </Form.Group>
                                 <Form.Group className={'mt-3'} as={Row}>
                                     <Form.Label column sm={3}>시작시간</Form.Label>
                                     <Col>
-                                        <Form.Select>
-                                            <option>전체</option>
+                                        <Form.Select defaultValue={'ALL'} onChange={handleChangeInputsTimetableId}>
+                                            <option value={'ALL'}>전체</option>
+                                            {timetables.map((timetable, timetableIdx) => {
+                                                return <option key={`option-timetable-${timetableIdx}`} value={timetable.value}>{timetable.name}</option>
+                                            })}
                                         </Form.Select>
                                     </Col>
                                 </Form.Group>
                                 <Form.Group className={'mt-3'} as={Row}>
                                     <Form.Label column sm={3}>좌석</Form.Label>
                                     <Col>
-                                        <Form.Select>
-                                            <option>전체</option>
+                                        <Form.Select defaultValue={'ALL'} onChange={handleChangeInputsSeat}>
+                                            <option value={'ALL'}>전체</option>
+                                            {seats.map((seat, seatIdx) => {
+                                                return <option key={`option-seat-${seatIdx}`} value={seat.value}>{seat.name}</option>
+                                            })}
                                         </Form.Select>
                                     </Col>
                                 </Form.Group>
@@ -63,11 +238,11 @@ const UserReservation = () => {
                             <Card.Title className={'font-HakDotR'}>Total</Card.Title>
                             <Stack className={'justify-content-between'} direction={'horizontal'}>
                                 <div>Amount</div>
-                                <div className={'h2'}>14,000</div>
+                                <div className={'h2'}>{price}</div>
                             </Stack>
                             <hr/>
                             <Stack>
-                                <Button variant={'dark'}>
+                                <Button variant={'dark'} onClick={handleClickPayment}>
                                     <div className={'h3'}>Payment</div>
                                 </Button>
                             </Stack>
