5564ef5d6949ec5f37cf46314b2c75a10a0527aa
jwjeong <timestar7979@gmail.com>
2024-06-01 14:41:51 +0900

    feat: 사용자 영화 상세 화면
    - 부제: 기능 구현
    - 설명: 영화 데이터 표출 및 예매하기 이동 구현

--- src/user/page/UserMoviesDetail.js
+++ src/user/page/UserMoviesDetail.js
@@ -1,26 +1,66 @@
 import {Button, Card, Col, Container, Form, Row, Stack} from "react-bootstrap";
+import {useNavigate, useParams} from "react-router";
+import {useMemo, useState} from "react";
+import apiMovie from "../../api/apiMovie";
 
 const UserMoviesDetail = () => {
+    const params = useParams();
+    const navigate = useNavigate();
+    const [movie, setMovie] = useState({});
+    const handleClickReservation = () => navigate('/reservation', {
+        state: {
+            movieId: movie.id
+        }
+    });
+    const getMovie = () => {
+        const _params = {
+            ...params
+        };
+        apiMovie.getDetail(_params)
+            .then(response => {
+                const { data } = response;
+                setMovie({
+                    id: data.result.id,
+                    title: data.result.title,
+                    age: data.result.age < 12 ? '전체이용가' : `${data.result.age}세`,
+                    director: data.result.director,
+                    releaseDate: data.result.releaseDate.split('T')[0],
+                    runningTime: `${data.result.runningTime}분`,
+                    genres: makeGenres(data.result.genres),
+                    detail: data.result.detail,
+                    imageUrl: data.result.attachment,
+                });
+            })
+            .catch(err => {
+                const { status, data } = err.response;
+                alert(`error: ${data.message} (${status})`);
+            });
+    };
+    const makeGenres = genres => genres.map(genre => genre.name).join(', ');
+    const init = () => {
+        getMovie();
+    };
+    useMemo(init, []);
     return (
         <Container>
             <Row className={'mt-5'}>
                 <Col md={4}>
                     <Card>
-                        <Card.Img src={'https://cdn.pixabay.com/photo/2023/11/22/16/08/generated-ai-8405812_1280.jpg'} />
+                        <Card.Img src={movie.imageUrl} />
                     </Card>
                 </Col>
                 <Col>
                     <Stack gap={5}>
                         <Stack style={{ fontSize: 60 }}>
-                            <p className={'font-HakDotR'}>Last City</p>
+                            <p className={'font-HakDotR'}>{movie.title}</p>
                         </Stack>
                         <Stack className={'h5'}>
-                            <p>Score: 4.5 / 5</p>
-                            <p>Genre: SF</p>
-                            <p>Age: 12</p>
-                            <p>Director: Benedict Benjamin</p>
-                            <p>Duration: 120m</p>
-                            <p>Released: 2024-04-10</p>
+                            <p>Score: {movie.score} / 5</p>
+                            <p>Genre: {movie.genres}</p>
+                            <p>Age: {movie.age}</p>
+                            <p>Director: {movie.director}</p>
+                            <p>Duration: {movie.runningTime}</p>
+                            <p>Released: {movie.releaseDate}</p>
                         </Stack>
                     </Stack>
                 </Col>
@@ -28,19 +68,14 @@ const UserMoviesDetail = () => {
             <Row className={'mt-5 h4'}>
                 <Col>
                     <div className={'bg-secondary-subtle p-3'} style={{ borderLeft: '5px solid #ccc' }}>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                        It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-                        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-                        and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                        {movie.detail}
                     </div>
                 </Col>
             </Row>
             <Row className={'mt-5'}>
                 <Col className={'d-flex justify-content-center'}>
-                    <Button className={'w-50'} variant={'dark'}>
-                        <div className={'h3 m-0 font-HakDotR'}>RESERVE</div>
+                    <Button className={'w-50'} variant={'dark'} onClick={handleClickReservation}>
+                        <div className={'h3 m-0 font-HakDotR'}>예매하기</div>
                     </Button>
                 </Col>
             </Row>
