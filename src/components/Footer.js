96cd50e269718e84be419957a0b40e179c782979
jwjeong <timestar7979@gmail.com>
2024-06-28 17:58:31 +0900

    fix: 푸터 웹 사이트명 불일치
    fixed #10

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
-        <footer>
-            <div className="font-HakDotR">MY MOVIE</div>
+        <footer className={'mt-5'}>
+            <div className="font-HakDotR">Simple Movie</div>
             <div>
                 <p>Copyright © 2024 mymovie.co.,Ltd. All rights reserved.</p>
                 <address>Contact webmaster for more information. 070-1234-5678</address>
