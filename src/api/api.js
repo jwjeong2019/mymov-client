ebf35b19cd2a1df43ca65ba497be618de07e4d6a
jwjeong <timestar7979@gmail.com>
2024-07-04 01:12:43 +0900

    modify: api 모듈
    1. api.js
    - Base URL에서 dev 제거

--- src/api/api.js
+++ src/api/api.js
@@ -5,7 +5,7 @@ const {
     REACT_APP_API_HOST,
     REACT_APP_API_PORT
 } = process.env;
-const BASE_URL = `${REACT_APP_API_PROTOCOL}://${REACT_APP_API_HOST}:${REACT_APP_API_PORT}/v1/api/dev`;
+const BASE_URL = `${REACT_APP_API_PROTOCOL}://${REACT_APP_API_HOST}:${REACT_APP_API_PORT}/v1/api`;
 
 const api = axios.create({
     baseURL: BASE_URL
